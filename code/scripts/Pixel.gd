8ce84ce6c04c6f4c0189d0e157ff211686713ee3
slowblink <iostara@pacsci.org>
2026-01-23 15:25:22 -0800

    changed pixel to texturerect

--- code/scripts/Pixel.gd
+++ code/scripts/Pixel.gd
@@ -1,13 +1,11 @@
-extends ColorRect
+extends TextureRect
 
 var undo_redo = UndoRedo.new() ## I think this initializes the undo function?
 var mouse_over : bool = false
 
-@export var history_buffer_size = 10 ##this value should match however many steps we're able to undo
-@export var initial_color : Color = Color(1,1,1,1)
+@export var current_color : Color = Color(1,1,1,1)
 
 @onready var color_history : Array[Color] ## holds the last 10 colors that this pixel node has been
-@onready var current_step : int = history_buffer_size - 1 ##track the current step of this pixel for undo and redo
 @onready var pixel_grid = get_parent()
 @onready var mask_animator = $MaskAnimator
 @onready var corner_position_a : Vector2
@@ -20,15 +18,12 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch or event is InputEventScreenDrag:
 		if event.position.x > global_position.x and event.position.x < global_position.x + size.x:
 			if event.position.y > global_position.y and event.position.y < global_position.y + size.y:
-				if Global.picked_color != get_color(): 
+				#if Global.picked_color != get_color():  FIXME: replace with a new function since this is no longer a colorrect
 					queueDraw()
 
 func checkTouchPosition():
 	pass
 
-func initializeColorHistory():
-	color_history.resize(history_buffer_size)
-	color_history.fill(initial_color)
 
 func queueDraw():##tells PixelGrid that this node needs to draw
 	pixel_grid.drawToPixel(self)
@@ -44,7 +39,8 @@ func _on_mouse_exited():
 	mouse_over = false
 
 func reset(): ##just sets the color back to default value. hardcoded black at the moment
-	set_color(initial_color)
+	pass
+	#set_color(initial_color) FIXME: replace with a new function since this is no longer a colorrect
 
 func getBoundaries():
 	corner_position_a = global_position
