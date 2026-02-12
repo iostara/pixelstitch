06be3a55cfbec14dc63bf80611a49398d59d41fb
slowblink <iostara@pacsci.org>
2026-02-12 11:52:57 -0800

    replacing pixel with tile_template

--- code/scripts/tile.gd
+++ code/scripts/tile.gd
@@ -1,1 +1,4 @@
-extends Node2D
+extends TextureRect
+
+var tile_index : int
+@export var tileswatch_library : Array[Resource] ##all of the tilesets in the for of their own arrays, each with its own index
