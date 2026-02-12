592372189f1717bc5d885b663b3869b578a8145e
slowblink <iostara@pacsci.org>
2026-02-12 12:23:51 -0800

    tile_palette rename

--- code/resources/tile_palette.gd
+++ code/resources/tile_palette.gd
@@ -1,5 +1,5 @@
 extends Resource
 
-class_name TileSwatch
+class_name TilePalette
 
 @export var texture_payload : Array[Texture2D]
--- code/scripts/tile.gd
+++ code/scripts/tile.gd
@@ -1,4 +1,4 @@
 extends TextureRect
 
-var tile_index : int
-@export var tileswatch_library : Array[Resource] ##all of the tilesets in the for of their own arrays, each with its own index
+@export var tile_index : int ##this determines which tile of the given tile_palette
+@export var tile_palette_library : Array[Resource] ##all of the tilesets in the for of their own arrays, each with its own index
