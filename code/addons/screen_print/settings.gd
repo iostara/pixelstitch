dc24734c43025732475ab71924e53082b93d1263
slowblink <iostara@pacsci.org>
2026-02-15 12:51:18 -0800

    added a debug tool

---
+++ code/addons/screen_print/settings.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Screen_Print_Settings
+## font size of screen messages
+@export var FONT_SIZE : int = 19
+## font size of screen messages
+@export var FONT_OUTLINE_SIZE : int = 5
+## should show just with editor
+@export var EDITOR_ONLY_MESSAGES : bool = true
+## prevents the printing.
+@export var HIDE_ALL_MESSAGES : bool = false
